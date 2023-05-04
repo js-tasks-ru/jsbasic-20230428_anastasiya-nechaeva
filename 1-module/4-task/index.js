1ef83f8a5d8b723dff09d37c6f482eac7e4b651f
AnastasiaNech <nechaevaanastasiamih@gmail.com>
2023-05-04 16:26:39 +0300

    Задачи 2-5

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,6 @@
 function checkSpam(str) {
-  // ваш код...
+  if(str.toLowerCase().indexOf('XXX'.toLowerCase()) != -1 || str.toLowerCase().indexOf('1xBet'.toLowerCase()) != -1)
+    return true;
+  return false;
 }
+
