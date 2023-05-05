f574e5e7e41a0be7ebe0daa8fc4e7d8821e909cc
AnastasiaNech <nechaevaanastasiamih@gmail.com>
2023-05-05 13:30:22 +0300

    Задачa 4

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,6 +1,4 @@
 function checkSpam(str) {
-  if(str.toLowerCase().indexOf('XXX'.toLowerCase()) != -1 || str.toLowerCase().indexOf('1xBet'.toLowerCase()) != -1)
-    return true;
-  return false;
+  return str.toLowerCase().indexOf('XXX'.toLowerCase()) != -1 || str.toLowerCase().indexOf('1xBet'.toLowerCase()) != -1;
 }
 
